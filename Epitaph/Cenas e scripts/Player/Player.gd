77f6eadce438a0b0bed1c97e7c06494406d91ed5
Diegusto <diegusto35@gmail.com>
2022-02-17 19:07:16 -0300

    animação de pulo adicionada

--- Epitaph/Cenas e scripts/Player/Player.gd
+++ Epitaph/Cenas e scripts/Player/Player.gd
@@ -5,13 +5,14 @@ const Cima = Vector2(0,-1)
 const Gravidade = 40
 const MaxVelocidadeQueda = 600
 const MaxVelociade = 300
-const ForcaPulo = 1100
+const ForcaPulo = 1000
 
 var movimento = Vector2()
 
 
 #função referente a movimentação
 func _physics_process(delta):
+	movimento = move_and_slide(movimento,Cima)
 	
 	movimento.y += Gravidade
 	if movimento.y > MaxVelocidadeQueda:
@@ -20,22 +21,29 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		movimento.x = MaxVelociade
 		
-		get_node("AnimatedSprite").animation = "andando"
 		get_node("AnimatedSprite").flip_h = false
+		if is_on_floor():
+			get_node("AnimatedSprite").animation = "andando"
+		
 		
 	elif Input.is_action_pressed("ui_left"):
 		movimento.x = -MaxVelociade
 		
-		get_node("AnimatedSprite").animation = "andando"
 		get_node("AnimatedSprite").flip_h = true
+		if is_on_floor():
+			get_node("AnimatedSprite").animation = "andando"
 	else:
 		movimento.x = 0
-		get_node("AnimatedSprite").animation ="default"
+		if is_on_floor():
+			get_node("AnimatedSprite").animation = "default"
 		#get_node serve para tratar a animação mudando de default para andando e mudando o lado de acordo com o input
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			movimento.y = -ForcaPulo
-		
-	movimento = move_and_slide(movimento,Cima)
+			
+	if !is_on_floor():
+				get_node("AnimatedSprite").animation = "pulando"
+
+	
 	
